94fc02acb0fcbab6859f088488cfe99dccd7ccc5
Rexa <zhaoweic18@gmail.com>
2017-03-30 22:20:19 +0900

    Create a project and add Header component

---
+++ exercises/minegi/ConvertTest/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Header } from './components/common';
+
+
+const App = () => {
+    return (
+        <View>
+            <Header headerText="ConvertTest" />
+        </View>
+    );
+};
+
+export default App;
